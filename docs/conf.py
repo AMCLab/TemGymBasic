bfe9c84cdae118dc6c571331c186e718510a03af
David-Landers <davidlanders2009@hotmail.com>
2022-09-22 17:30:32 +0100

    editing conf

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,6 @@ exclude_patterns = []
 # The suffix of source filenames.
 source_suffix = '.rst'
 
-
 # The master toctree document.
 master_doc = 'index'
 
@@ -59,9 +58,20 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'default'
+
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = project+'doc'
