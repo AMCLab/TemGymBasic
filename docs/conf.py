1bb6db2195466d5e956d8b3ec13c4b0cac0b5390
David-Landers <davidlanders2009@hotmail.com>
2022-10-10 23:37:38 +0100

    added requirement.txt

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'sphinx_licenseinfo']
+    'sphinx.ext.sphinx_licenseinfo']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
